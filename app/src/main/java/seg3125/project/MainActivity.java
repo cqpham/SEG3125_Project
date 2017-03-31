507b16c354ec8d202af2c658e83f0f39034b0fe8
cqpham <cpham021@uottawa.ca>
2017-03-31 12:39:49 -0400

    Added dialog for FAB

--- app/src/main/java/seg3125/project/MainActivity.java
+++ app/src/main/java/seg3125/project/MainActivity.java
@@ -2,13 +2,18 @@ package seg3125.project;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.support.v7.app.AlertDialog.Builder;
 
 public class MainActivity extends AppCompatActivity {
+
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showDialog();
             }
         });
     }
@@ -32,5 +36,35 @@ public class MainActivity extends AppCompatActivity {
         //Inflate the menu -> this adds items to the action bar
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Dialog Title");
+        builder.setMessage("Dialog Message");
+
+        String positiveText = getString(android.R.string.ok);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // positive button logic
+                        dialog.cancel();
+                    }
+                });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // negative button logic
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        // display dialog
+        dialog.show();
     }
 }
